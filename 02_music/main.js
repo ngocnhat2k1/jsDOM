a3e24211b1af176983ea0b1c33e1dcc7b2fab0ff
ngocnhat2k1 <ngocnhat2k1@gmail.com>
2021-05-18 20:57:19 +0700

    add all file

---
+++ 02_music/main.js
@@ -0,0 +1,35 @@
+function playsoundsC() {
+    var soundC = document.getElementById('C')
+    soundC.currentTime = 0;
+    soundC.play();
+}
+function playsoundsD() {
+    var soundD = document.getElementById('D')
+    soundD.currentTime = 0;
+    soundD.play();
+}
+function playsoundsE() {
+    var sounde = document.getElementById('E')
+    sounde.currentTime = 0;
+    sounde.play();
+}
+function playsoundsF() {
+    var soundF = document.getElementById('F')
+    soundF.currentTime = 0;
+    soundF.play();
+}
+function playsoundsG() {
+    var soundG = document.getElementById('G')
+    soundG.currentTime = 0;
+    soundG.play();
+}
+function playsoundsA() {
+    var soundA = document.getElementById('A')
+    soundA.currentTime = 0;
+    soundA.play();
+}
+function playsoundsB() {
+    var soundA = document.getElementById('B')
+    soundA.currentTime = 0;
+    soundA.play();
+}
